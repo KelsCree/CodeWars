0b539b4db707c7d7da8d2fd5336d01d646b8686c
KelsCree <kelseycreehan@gmail.com>
2021-01-18 16:25:35 -0700

    Add one-liner mumbling kata solution.

--- Javascript/mumbling.js
+++ Javascript/mumbling.js
@@ -5,3 +5,8 @@
 // accum("abcd") -> "A-Bb-Ccc-Dddd"
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+//one line solution
+function accum(s) {
+  return [...s].map((char, index) => (char.toUpperCase() + char.toLowerCase().repeat(index))).join("-")
+}
