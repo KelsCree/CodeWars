f285c3ba5859538d6259d0170254f4c102589438
KelsCree <kelseycreehan@gmail.com>
2021-01-18 17:10:13 -0700

    Solve anagrams kata.

--- Javascript/anagrams.js
+++ Javascript/anagrams.js
@@ -8,3 +8,13 @@
 
 // 'abba' & 'abca' == false
 // Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none.
+
+function anagrams(word, words) {
+  let matches = []
+  let sortedWord = word.split('').sort().join('')
+  for (let i = 0; i < words.length; i++) {
+    let sortedIndex = words[i].split('').sort().join('')
+    sortedIndex == sortedWord ? matches.push(words[i]) : null
+  }
+  return matches
+}
